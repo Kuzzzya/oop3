6ed728a3d82a3252380a7a6867e68c321b483d63
Kuzzzya <chelovechin@mail.ru>
2022-03-14 10:49:14 +0300

    first commit

---
+++ src/main/java/ru/netology/oop2/FormDate.java
@@ -0,0 +1,7 @@
+package ru.netology.oop2;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+}
---
+++ src/main/java/ru/netology/oop2/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.oop2;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronomyc;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+}
